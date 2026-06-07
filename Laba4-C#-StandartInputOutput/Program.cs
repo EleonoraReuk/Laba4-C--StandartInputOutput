203e034ee68cb37e0575ae4d3ad93d51c87f9e30
Eleonor2020 <reuk20077@mail.ru>
2026-06-07 19:36:53 +0700

    class TextFile

--- Laba4-C#-StandartInputOutput/Program.cs
+++ Laba4-C#-StandartInputOutput/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class TextFile
+{
+    public string FileName { get; set; }
+    public string Content { get; set; }
+    public TextFile() { }
+    public TextFile(string fileName, string content)
+    {
+        FileName = fileName; 
+        Content = content;
+    }
+}
