b3b37d19e0e410b16c073e646a50de0f15866d5a
Eleonor2020 <reuk20077@mail.ru>
2026-06-07 19:45:13 +0700

    XML

--- Laba4-C#-StandartInputOutput/Program.cs
+++ Laba4-C#-StandartInputOutput/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Xml.Serialization;
 
 public class TextFile
 {
@@ -10,4 +12,18 @@ public class TextFile
         FileName = fileName; 
         Content = content;
     }
+
+    public void SaveXml(string path)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(TextFile));
+        using FileStream fs = new FileStream(path, FileMode.Create);
+        serializer.Serialize(fs, this);
+    }
+    public static TextFile LoadXml(string path)
+    {
+        XmlSerializer serializer = new XmlSerializer (typeof(TextFile));
+        using FileStream fs = new FileStream(path, FileMode.Open);
+        return (TextFile)serializer.Deserialize(fs);
+    }
 }
+
