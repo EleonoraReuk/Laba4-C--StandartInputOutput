e47dc420c7b5081efdabca3c251949f7b9bace7d
Eleonor2020 <reuk20077@mail.ru>
2026-06-07 20:19:07 +0700

    Бинарная реализация

--- Laba4-C#-StandartInputOutput/Program.cs
+++ Laba4-C#-StandartInputOutput/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.IO;
 using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 
+[Serializable]
 public class TextFile
 {
     public string FileName { get; set; }
@@ -24,6 +26,25 @@ public class TextFile
         XmlSerializer serializer = new XmlSerializer (typeof(TextFile));
         using FileStream fs = new FileStream(path, FileMode.Open);
         return (TextFile)serializer.Deserialize(fs);
+    }
+
+
+    public void SaveBinary(string path)
+    {
+        using FileStream fs = new FileStream(path, FileMode.Create);
+#pragma warning disable SYSLIB0011
+        BinaryFormatter formatter = new BinaryFormatter();
+        formatter.Serialize(fs, this);
+#pragma warning restore SYSLIB0011
+    }
+
+    public static TextFile LoadBinary(string path)
+    {
+        using FileStream fs = new FileStream (path, FileMode.Open);
+#pragma warning disable SYSLIB0011
+        BinaryFormatter formatter = new BinaryFormatter();
+        return (TextFile)formatter.Deserialize(fs);
+#pragma warning restore SYSLIB0011
     }
 }
 
