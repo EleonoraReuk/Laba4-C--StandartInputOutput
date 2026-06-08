9f85a4fe5c349d6f6dcd25558fcc9b12f08d19c0
Eleonor2020 <reuk20077@mail.ru>
2026-06-08 12:03:56 +0700

    Редактор TextEditor

---
+++ Laba4-C#-StandartInputOutput/TextEditor.cs
@@ -0,0 +1,12 @@
+﻿public class TextEditor
+{
+    public string Content { get; set; }
+    public TextMemento Save()
+    {
+        return new TextMemento(Content);
+    }
+    public void Restore(TextMemento memento)
+    {
+        Content = memento.State;
+    }
+}
